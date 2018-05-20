dd7138e928c0be419caa793ff99c30db0a444c03
arturarushanyan <arthur.arushanyan92@gmail.com>
2018-05-20 15:22:45 +0400

    Added modal and dynamic labels for markers

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,99 @@
 import React, { Component } from 'react';
 import GoogleMap from './GoogleMap';
+import Modal from 'react-modal';
+import shortID from 'short-id-gen';
 import '../App.css';
+
+const customStyles = {
+    content : {
+        top                   : '50%',
+        left                  : '50%',
+        right                 : 'auto',
+        bottom                : 'auto',
+        marginRight           : '-50%',
+        transform             : 'translate(-50%, -50%)'
+    }
+};
 
 class App extends Component {
     constructor(props){
         super(props);
         this.handleClick = this.handleClick.bind(this);
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+        this.onModalInputChange = this.onModalInputChange.bind(this);
+        this.confirmAddPlace = this.confirmAddPlace.bind(this);
+        this.cancelAddPlace = this.cancelAddPlace.bind(this);
         this.state = {
-            markers: []
+            markers: [],
+            modalIsOpen: false,
+            modalInputValue: '',
+            currentMarkerID: null
         }
     }
     handleClick(e) {
-        let lat = e.latLng.lat(),
-            lng = e.latLng.lng()
-        console.log(lat,lng);
+        const lat = e.latLng.lat(),
+            lng = e.latLng.lng(),
+            markerID = shortID.generate();
+
         this.setState({
+            currentMarkerID: markerID,
+            modalIsOpen: true,
             markers: [
                 ...this.state.markers,
-                { lat, lng }
+                {
+                    id: markerID,
+                    label: '',
+                    position: { lat, lng },
+                    labelAnchor: new window.google.maps.Point(0, 60)
+                }
+            ]
+        });
+    }
+
+    confirmAddPlace() {
+        const currentMarker = this.state.markers.filter(marker => marker.id === this.state.currentMarkerID)[0];
+        const otherMarkers = this.state.markers.filter(marker => marker.id !== this.state.currentMarkerID);
+        const updatedMarker = Object.assign({}, currentMarker, {label: this.state.modalInputValue});
+        this.setState({
+            modalIsOpen: false,
+            markers: [
+                ...otherMarkers,
+                updatedMarker
+            ]
+        });
+    }
+
+    cancelAddPlace() {
+        const filteredMarkers = this.state.markers.filter(marker => marker.id !== this.state.currentMarkerID);
+        this.setState({
+            modalIsOpen: false,
+            markers: [
+                ...filteredMarkers
             ]
         })
     }
+
+    openModal() {
+        this.setState({modalIsOpen: true});
+    }
+
+    onModalInputChange(e){
+        const inputValue = e.target.value;
+        console.log(inputValue);
+        this.setState({
+            modalInputValue: inputValue
+        })
+    }
+
+    closeModal() {
+        this.setState({modalIsOpen: false});
+    }
+
+    componentWillMount() {
+        Modal.setAppElement('body');
+    }
+
     render() {
     return (
         <div>
@@ -32,6 +105,17 @@ class App extends Component {
                        handleClick={this.handleClick}
                        markers={this.state.markers}
             />
+            <Modal
+                isOpen={this.state.modalIsOpen}
+                onRequestClose={this.closeModal}
+                style={customStyles}
+                contentLabel="Places Modal"
+            >
+                <h4>Name Your selected place</h4>
+                <input type="text" onChange={this.onModalInputChange} value={this.state.modalInputValue}/>
+                <button onClick={this.confirmAddPlace}>Confirm</button>
+                <button onClick={this.cancelAddPlace}>Cancel</button>
+            </Modal>
         </div>
     )
     }
--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
+import { MarkerWithLabel } from 'react-google-maps/lib/components/addons/MarkerWithLabel';
+
 
 export default withScriptjs(withGoogleMap((props) => (
         <GoogleMap
-            defaultZoom={8}
-            defaultCenter={{ lat: -34.397, lng: 150.644 }}
+            defaultZoom={16}
+            defaultCenter={{ lat: 40.17956771440295, lng: 44.51704104658768 }}
             onClick={(e) => props.handleClick(e)}
         >
             {
                 props.markers.map((marker, index) => {
                     return (
-                        <Marker key={index} position={marker} />
+                        <MarkerWithLabel
+                            key={index}
+                            position={marker.position}
+                            labelAnchor={marker.labelAnchor}
+                            labelStyle={{backgroundColor: "yellow", fontSize: "13px", padding: "5px"}}
+                        >
+                            <div>{marker.label}</div>
+                        </MarkerWithLabel>
                     )
                 })
             }
